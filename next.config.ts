435b60129292fefcaae518ab6e8f92f427a27da7
Miguel Uzcategui <gomezuzcategui20@gmail.com>
2025-09-11 09:50:09 -0500

    Deploy site to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,12 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/tayroPharma',
+  assetPrefix: '/tayroPharma', // 👉 Esto es CRUCIAL para GitHub Pages
   images: {
     unoptimized: true,
   },
   eslint: {
-    ignoreDuringBuilds: true, // 👈 ignorar errores de ESLint al compilar
+    ignoreDuringBuilds: true,
   },
 };
 

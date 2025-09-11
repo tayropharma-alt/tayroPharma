3fcd4a88570a18df725385ff611920f512777a34
Miguel Uzcategui <gomezuzcategui20@gmail.com>
2025-09-11 13:53:16 -0500

    config: static export for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/tayroPharma',
-  assetPrefix: '/tayroPharma/',
+  output: 'export'
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
+
